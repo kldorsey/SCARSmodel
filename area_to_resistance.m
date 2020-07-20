1ab3eb57f60a94042ba98f7dd0126dcbdcc44a71
kldorsey <52797811+kldorsey@users.noreply.github.com>
2020-07-20 12:19:29 -0400

    Add files via upload

--- area_to_resistance.m
+++ area_to_resistance.m
@@ -1,17 +1,16 @@
-
-
 function serpentine_res = area_to_resistance(num_beams,b,Rs,Rl,Rc,L_eff)
     %num_beams,Rs,Rl,rho_c,xi_int,b,contact_area
 
     contact_length = b-L_eff; %find contact length between beams
     num_meanders = num_beams; %Mirror the serpentine
     
-    Rc_int = 1e-6;
+    Rc_int = 1e-6; %Calculates beam resistance in discrete sections-- size of each section
     
-    R_eff = ones(1,num_beams)*5e4;
+    R_eff = ones(1,num_beams)*5e4; %Assumes large resistance to allocate array
     
     for beams = 1:num_beams
         if contact_length(beams) > 0 
+        	%if beams are in contact
             num_ints = floor(contact_length(beams)/Rc_int);
 
             Rcp = Rc/Rc_int;
@@ -25,14 +24,14 @@ function serpentine_res = area_to_resistance(num_beams,b,Rs,Rl,Rc,L_eff)
             R_eff(beams) = Reff_iterate;
         end   
     end
-    contact_res =[fliplr(R_eff(2:num_beams)) R_eff(1) R_eff(2:num_beams)]; %Mirror the effective length matrix
+    contact_res =[fliplr(R_eff(2:num_beams)) R_eff(1) R_eff(2:num_beams)]; %Beam symmetry 
     serpentine_res = res_network(Rs,Rl,contact_res,num_meanders);
 end
 
-%Use contact resistance array to find total serpentine resistance
 function serpentine_res = res_network(Rs,Rl,Rc_array,num_meanders)
-%inputs are Rs, Rl, Rc array, and num_meanders. output is total resistance
-%for array of contact resistances, find the total serpentine resistance
+	%Use contact resistance array to find total serpentine resistance
+	%inputs are Rs, Rl, Rc array, and num_meanders. output is total resistance
+	%for array of contact resistances, find the total serpentine resistance
 
     num_nodes = (num_meanders-2)*4+5;
 
@@ -51,11 +50,11 @@ function serpentine_res = res_network(Rs,Rl,Rc_array,num_meanders)
             G(k,k+2) = 1/Rc_array(k); %first upper off_diagonal
         elseif k == 2
             G(k-1,k) = 1/(Rl+Rs);
-            G(k,k) = -(1/(Rl+Rs)+1/Rc_array(k)+1/Rl); %%%%%%
+            G(k,k) = -(1/(Rl+Rs)+1/Rc_array(k)+1/Rl); 
             G(k+1,k) = 1/Rl;
         elseif k == num_nodes
             G(k-1,k) =1/Rl;
-            G(k,k) = -(1/(Rl+Rs)+1/Rc_array(contact_res_index)+1/Rl); %%%%%
+            G(k,k) = -(1/(Rl+Rs)+1/Rc_array(contact_res_index)+1/Rl); 
         else
             if mod(k,2) == 1
                 G(k,k) = -(1/Rs+1/Rc_array(contact_res_index)+1/Rl); %Contact resistance changes even/odd by 2
@@ -76,13 +75,13 @@ function serpentine_res = res_network(Rs,Rl,Rc_array,num_meanders)
         end
     end
 
-    %Assume an input test current of 1 A
+    %Assume an input test current of 1 A for nodal analysis
     I_test = zeros(num_nodes,1);
     I_test(1) = -1;
 
     %Solve for node voltages using conductance matrix and current
     node_voltages = G\I_test;
-    serpentine_res = node_voltages(1);
+    serpentine_res = node_voltages(1); %Find effective/Thevenin resistance
 end
 
 
